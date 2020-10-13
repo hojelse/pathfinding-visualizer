8ca79696a953ee524365f1f4f9445374ec4ae49b
hojelse <k1@hojelse.dk>
2020-10-13 22:45:49 +0200

    Create SVG to replace css grid board

--- src/App.tsx
+++ src/App.tsx
@@ -1,129 +1,47 @@
 import React, { useRef, useState, useEffect } from "react"
 import "./App.css"
+import { Graph } from "./Dijkstra/Graph"
 
 let App = () => {
-  const meRef = useRef(null) // default value: null
-
-  const [dims, setDims] = useState([0, 0])
-  useEffect(() => {
-    const resizeObserver = new ResizeObserver((entries) => {
-      let entry = entries[0]
-      const { width, height } = entry.contentRect
-      setDims([width, height])
-    })
-    let me = meRef.current
-    if (me) resizeObserver.observe(me)
-    return () => me && resizeObserver.unobserve(me)
-  }, [meRef.current])
-
   return (
     <div
-      ref={meRef}
-      className="App"
       style={{
         width: "100vw",
         height: "100vh",
         display: "flex",
         alignItems: "center",
-        flexDirection: "column",
         justifyContent: "center",
+        padding: "100px",
       }}
     >
-      {/* rows and columns must be uneven */}
-      <Board dims={dims} showMST={true} rows={9} columns={9} />
+      <GraphTracer></GraphTracer>
     </div>
   )
 }
 
-function Board({ rows, columns, dims }: any) {
-  const minDim = Math.min(dims[0], dims[1])
+let GraphTracer = () => {
+  let graph = new Graph(3)
+  graph.addEdge(0, 1)
+  graph.addEdge(0, 2)
+  graph.addEdge(1, 2)
 
   return (
-    <div
+    <svg
+      viewBox="-100 -100 200 200"
       style={{
-        height: `${minDim}px`,
-        width: `${minDim}px`,
-        position: "relative",
-        flexShrink: 0,
-        flexGrow: 0,
-        backgroundColor: "#222222",
-        boxShadow: "0 10px 20px -7px #00000044",
-        transform: "rotateX(60deg) rotateY(0deg) rotateZ(-45deg)",
+        height: "100%",
+        width: "100%",
+        border: "1px solid white",
       }}
     >
-      {/* Nodes */}
-      <div
-        style={{
-          padding: "20px",
-          position: "absolute",
-          height: "100%",
-          width: "100%",
-          display: "grid",
-          gridTemplateColumns: [...new Array(columns)]
-            .map((_, i) => (i % 2 === 0 ? "10fr" : "minmax(5px, 1fr)"))
-            .join(" "),
-          gridTemplateRows: [...new Array(rows)]
-            .map((_, i) => (i % 2 === 0 ? "10fr" : "minmax(5px, 1fr)"))
-            .join(" "),
-          alignItems: "center",
-          justifyItems: "center",
-          // gap: "10px",
-        }}
-      >
-        {[...new Array(rows * columns)].map((_, i) => {
-          const isOddRow = i % (rows * 2) < rows
-          const isOddCol = (i % columns) % 2 === 0
-          if (isOddCol && isOddRow) return <Node key={i} />
-          if (!isOddCol !== !isOddRow) {
-            if (isOddCol) return <Edge key={i} vertical={true} />
-            return <Edge key={i} vertical={false} />
-          }
-          return <Gap key={i} />
-        })}
-      </div>
-    </div>
+      <path className="edge" d="M0,0 L 30,0" />
+      <path className="edge" d="M30,0 L 30,40" />
+      <path className="edge" d="M30,40 L 0,0" />
+      <circle className="vertex" cx="0" cy="0" />
+      <circle className="vertex" cx="30" cy="0" />
+      <circle className="vertex" cx="30" cy="40" />
+    </svg>
   )
-}
-
-function Node(props) {
-  return (
-    <div
-      {...props}
-      style={{
-        height: "100%",
-        width: "100%",
-        backgroundColor: "#383838",
-      }}
-    ></div>
-  )
-}
-
-function Edge({ vertical }) {
-  return (
-    <div
-      className={vertical ? "edgeVertical" : "edgeHorizontal"}
-      style={{
-        height: "100%",
-        width: "100%",
-        backgroundColor: "#2d2d2d",
-      }}
-    ></div>
-  )
-}
-
-function Gap() {
-  return (
-    <div
-      style={{
-        height: "100%",
-        width: "100%",
-      }}
-    ></div>
-  )
-}
-
-function createInitialState(rowsCount: number, cols: number) {
-  // [new Array(rowsCount)]
 }
 
 export default App
