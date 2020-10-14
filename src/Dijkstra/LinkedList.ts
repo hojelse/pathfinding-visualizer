e5b6482ea49161ac557a6f489949e45ae4617334
Kristoffer Højelse <info@hojelse.com>
2020-10-14 15:05:49 +0200

    Fix constructor

--- src/Dijkstra/LinkedList.ts
+++ src/Dijkstra/LinkedList.ts
@@ -23,8 +23,8 @@ class LinkedListNode<T> {
   data: T;
   next: LinkedListNode<T> | null;
   constructor(data: T, next: LinkedListNode<T> | null = null) {
-    this.data = data,
-      this.next = next
+    this.data = data;
+    this.next = next;
   }
 }
 
