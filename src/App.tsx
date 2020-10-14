32d0f4cbadf462ed165bebb50791b73c6f4a48e8
Kristoffer Højelse <info@hojelse.com>
2020-10-14 17:00:46 +0200

    Create constructor functions for edges and vertices

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useRef, useState, useEffect } from "react"
 import "./App.css"
+import { Edge } from "./Dijkstra/Edge"
 import { Graph } from "./Dijkstra/Graph"
+import { UndirectedGraph } from "./Dijkstra/UndirectedGraph"
 
 let App = () => {
   return (
@@ -20,10 +22,27 @@ let App = () => {
 }
 
 let GraphTracer = () => {
-  let graph = new Graph(3)
-  graph.addEdge(0, 1)
-  graph.addEdge(0, 2)
-  graph.addEdge(1, 2)
+  let n = 3;
+
+  let graph = new UndirectedGraph(n);
+  graph.addEdge(0, 1);
+  graph.addEdge(0, 2);
+  graph.addEdge(1, 2);
+
+
+  let vertexPositions = [
+    { x: 0, y: 0 },
+    { x: 30, y: 0 },
+    { x: 30, y: 40 },
+  ]
+
+  for (let i = 0; i < n; i++) {
+    for (const edge of graph.getNeigborhoodOf(i)) {
+      let from = vertexPositions[edge.from];
+      let to = vertexPositions[edge.to];
+      return <path className="edge" d={`M${from.x},${from.y} L ${to.x},${to.y}`} />
+    }
+  }
 
   return (
     <svg
@@ -37,9 +56,9 @@ let GraphTracer = () => {
       <path className="edge" d="M0,0 L 30,0" />
       <path className="edge" d="M30,0 L 30,40" />
       <path className="edge" d="M30,40 L 0,0" />
-      <circle className="vertex" cx="0" cy="0" />
-      <circle className="vertex" cx="30" cy="0" />
-      <circle className="vertex" cx="30" cy="40" />
+      {[...vertexPositions].map((point, i) => {
+        return <circle className="vertex" cx={point.x} cy={point.y} />
+      })}
     </svg>
   )
 }
